ddc6c57516cba987972c4216c24fa09addaa4ce1
10xdev4u-alt <10xdev4u@gmail.com>
2026-06-28 01:32:57 +0530

    feat: add app constants

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const API_BASE = "http://127.0.0.1:4197";
+export const APP_NAME = "AetherSelf Demo";
+export const VERSION = "0.1.0";
+export const DEFAULT_THEME = "dark" as const;
+export const MAX_MESSAGES = 1000;
+export const TYPING_DELAY_MS = 500;
